6aacf88ff2956a1ccd216fa0ba11fb2320e5a36e
shafiqahmad81 <shafiqahmad081@gmail.com>
2025-08-13 17:57:05 +0600

    complete tb trucking project

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,21 @@ const close = document.getElementById("close");
   close.addEventListener('click', () => {
     menu.classList.add('hidden'); // hide menu
   });
+
+  const swiper = new Swiper('.testimonial-slider', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
