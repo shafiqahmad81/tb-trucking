4ac79bce4ad7c81fe9e13611a0722ac0ae7e46a7
shafiqahmad81 <shafiqahmad081@gmail.com>
2025-08-15 17:54:18 +0600

    update header section

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,32 @@ const menu = document.getElementById("main-menu");
 const hamburgerBtn = document.getElementById("menu-btn");
 const close = document.getElementById("close");
 
+// header section
+
+window.addEventListener('scroll', function() {
+  const header = document.getElementById('header');
+  const bannerWrap = document.querySelector('.banar-wrap');
+  const banner = document.querySelector('.banar-content'); 
+   // banner content selector
+
+  if (window.scrollY > 0) {
+    header.classList.add('sticky');
+    bannerWrap.classList.add('scrolled');
+    banner.classList.add('scrolled'); // scroll হলে margin কমবে
+  } else {
+    header.classList.remove('sticky');
+    bannerWrap.classList.remove('scrolled');
+    banner.classList.remove('scrolled'); // scroll top এ margin আবার বাড়বে
+  }
+});
  hamburgerBtn.addEventListener('click', () => {
-    menu.classList.remove('hidden'); // show menu
+    menu.classList.remove('menu-hidden'); // show menu
+    close.classList.remove('close-hidden'); // show menu
   });
 
   close.addEventListener('click', () => {
-    menu.classList.add('hidden'); // hide menu
+    menu.classList.add('menu-hidden');
+    close.classList.add('close-hidden'); // hide menu
   });
 
   const swiper = new Swiper('.testimonial-slider', {
@@ -31,13 +51,13 @@ const close = document.getElementById("close");
   
   
 document.getElementById("loadMoreBtn").addEventListener("click", function () {
-  // service-area-wrap এর ভিতরের সব card-hidden element নাও
   const hiddenItems = document.querySelectorAll(".service-area-wrap .card-hidden");
   
   hiddenItems.forEach(function (item) {
     item.classList.remove("card-hidden");
   });
 
-  // সব দেখানোর পর Load More button hide করে দাও
   this.style.display = "none";
 });
+
+
